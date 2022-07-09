5f79876e2336464b5f2a3625f23f3f22952b5617
Aldiransyah Rizky Putra <aldiransyah.rizky@gmail.com>
2022-07-09 16:56:15 +0700

    add test case TC-Remove Product From Cart with the objects

---
+++ Object Repository/Add To Cart/Page Cart/txt_Your cart is currently empty.rs
@@ -0,0 +1,115 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>txt_Your cart is currently empty</name>
+   <tag></tag>
+   <elementGuidId>1c922ab0-adb0-44db-ab59-b80c8edd65a8</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//article[@id='post-53']/div/div/p</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>p.cart-empty</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>p</value>
+      <webElementGuid>1f28ff34-ab1f-41f0-9bd5-f6a885fdf1e4</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>cart-empty</value>
+      <webElementGuid>797a33f5-d8b5-4b75-8b92-ea58561a848c</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Your cart is currently empty.</value>
+      <webElementGuid>ca7b3cb4-2675-4443-b950-0b968ecc84fd</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;post-53&quot;)/div[@class=&quot;entry-content&quot;]/div[@class=&quot;woocommerce&quot;]/p[@class=&quot;cart-empty&quot;]</value>
+      <webElementGuid>454b783f-ddfd-407e-9ba8-4f8dab496175</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//article[@id='post-53']/div/div/p</value>
+      <webElementGuid>73c1646a-a91a-46d0-9f60-a2e7d7062bc9</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Undo?'])[1]/following::p[1]</value>
+      <webElementGuid>d81cfc8a-a3ce-46db-9332-f9bda639501e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Cart'])[1]/following::p[2]</value>
+      <webElementGuid>6fb85253-47a1-4395-a0b2-e5332b428574</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='You might like these'])[1]/preceding::p[1]</value>
+      <webElementGuid>563f6b42-d69a-4ddc-a6e1-42e4685b15fd</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='New!'])[1]/preceding::p[1]</value>
+      <webElementGuid>f5be3706-59b7-4348-a3bc-7be02efcd94b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Your cart is currently empty.']/parent::*</value>
+      <webElementGuid>5ddc26ff-ff13-4f65-8c76-66305ab1fb2e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div/p</value>
+      <webElementGuid>c3897a8e-20be-4484-b73b-e927ebfd7ca7</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//p[(text() = 'Your cart is currently empty.' or . = 'Your cart is currently empty.')]</value>
+      <webElementGuid>4382f5bf-f1b9-42dc-ae77-624f36eac132</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
